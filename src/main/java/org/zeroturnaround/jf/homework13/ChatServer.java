05c05f74e1ff4e4d8bfb97abd9c152519b3cc228
Pankhuri-14 <59799387+Pankhuri-14@users.noreply.github.com>
2020-01-12 21:12:09 +0530

    Update ChatServer.java
    ChatServer implementation

--- src/main/java/org/zeroturnaround/jf/homework13/ChatServer.java
+++ src/main/java/org/zeroturnaround/jf/homework13/ChatServer.java
@@ -1,9 +1,46 @@
 package org.zeroturnaround.jf.homework13;
 
-public class ChatServer {
+import java.io.*; 
+import java.text.*; 
+import java.util.*; 
+import java.net.*; 
 
-  public static void main(String[] args) {
-    // TODO implement
-  }
+public class ChatServer 
+{ 
+	public static void main(String[] args) throws IOException 
+	{ 
+		// server is listening on port 7070
+		ServerSocket ss = new ServerSocket(7070); 
+		
+		// running infinite loop for getting 
+		// client request 
+		while (true) 
+		{ 
+			Socket s = null; 
+			
+			try
+			{ 
+				// socket object to receive incoming client requests 
+				s = ss.accept(); 
+				
+				System.out.println(s +" has joined."); 
+				
+				// obtaining input and out streams 
+				DataInputStream dis = new DataInputStream(s.getInputStream()); 
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+				
 
-}
+				// create a new thread object 
+				Thread t = new ClientHandler(s, dis, dos); 
+
+				// Invoking the start() method 
+				t.start(); 
+				
+			} 
+			catch (Exception e){ 
+				s.close(); 
+				e.printStackTrace(); 
+			} 
+		} 
+	} 
+} 
